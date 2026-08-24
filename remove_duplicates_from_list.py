bc80c3b78b91c89ce49e68b30633423400afba78
Anindita Chanak <aninditachanak001@gmail.com>
2026-08-24 22:11:17 +0530

    Create remove_duplicates_from_list.py

---
+++ remove_duplicates_from_list.py
@@ -0,0 +1,10 @@
+numbers = [10, 20, 10, 30, 20, 40, 30]
+
+unique = []
+
+for num in numbers:
+    if num not in unique:
+        unique.append(num)
+
+print("Original list:", numbers)
+print("List after removing duplicates:", unique)
