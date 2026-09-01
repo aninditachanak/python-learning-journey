1ef1f2035a288a5f38f668109e8147d5f7efbae6
Anindita Chanak <aninditachanak001@gmail.com>
2026-09-02 00:19:05 +0530

    Create mean_median_mode.py

---
+++ mean_median_mode.py
@@ -0,0 +1,12 @@
+import statistics
+
+data = [10, 20, 20, 30, 40, 50, 60]
+
+mean = statistics.mean(data)
+median = statistics.median(data)
+mode = statistics.mode(data)
+
+print("Data:", data)
+print("Mean:", mean)
+print("Median:", median)
+print("Mode:", mode)
