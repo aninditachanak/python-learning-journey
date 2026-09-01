2fd4f5cb0448ecc58b3873aa459b3a1978cdf05f
Anindita Chanak <aninditachanak001@gmail.com>
2026-09-02 00:19:48 +0530

    Create range_of_data.py

---
+++ range_of_data.py
@@ -0,0 +1,11 @@
+data = [10, 20, 15, 40, 25, 30, 50]
+
+maximum = max(data)
+minimum = min(data)
+
+range_value = maximum - minimum
+
+print("Data:", data)
+print("Maximum:", maximum)
+print("Minimum:", minimum)
+print("Range:", range_value)
