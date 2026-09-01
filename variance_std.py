a0d5e552be495c5bd51ada0b0f126663256ebe5e
Anindita Chanak <aninditachanak001@gmail.com>
2026-09-02 00:21:55 +0530

    Update variance_std.py

--- variance_std.py
+++ variance_std.py
@@ -2,19 +2,15 @@ import math
 
 data = [10, 20, 30, 40, 50]
 
-# Calculate mean
 mean = sum(data) / len(data)
 
-# Calculate squared differences
 squared_sum = 0
 
 for x in data:
     squared_sum += (x - mean) ** 2
 
-# Calculate variance
 variance = squared_sum / (len(data) - 1)
 
-# Calculate standard deviation
 standard_deviation = math.sqrt(variance)
 
 print("Data:", data)
