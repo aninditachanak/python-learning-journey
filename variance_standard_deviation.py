f8055f089e80632dbb0ff6e7c4379dd5f72c7a53
Anindita Chanak <aninditachanak001@gmail.com>
2026-09-02 00:20:29 +0530

    Create variance_standard_deviation.py

---
+++ variance_standard_deviation.py
@@ -0,0 +1,10 @@
+import statistics
+
+data = [10, 20, 30, 40, 50]
+
+variance = statistics.variance(data)
+standard_deviation = statistics.stdev(data)
+
+print("Data:", data)
+print("Variance:", variance)
+print("Standard Deviation:", standard_deviation)
