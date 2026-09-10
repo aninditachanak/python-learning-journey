cf75a24e1e2ed47dd1007e8a26767d819efee723
Anindita Chanak <aninditachanak001@gmail.com>
2026-09-10 08:13:07 +0530

    Create probability.py

---
+++ probability.py
@@ -0,0 +1,8 @@
+favorable_outcomes = 3
+total_outcomes = 6
+
+probability = favorable_outcomes / total_outcomes
+
+print("Favorable Outcomes:", favorable_outcomes)
+print("Total Outcomes:", total_outcomes)
+print("Probability:", probability)
